c00fe0fa8cefc50029a055cf8c5854c1ea131c1a
Thorsten Zoerner <zoerner@gmail.com>
2017-11-22 01:44:59 +0000

    Added md

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ npm install -g fury.network
 npm install --save stromdao-businessobject
 npm install -g browserify
 
+mkdir public
 cd public
 git clone https://github.com/energychain/fury.mithrill
 git clone https://github.com/energychain/fury.erzeuger
